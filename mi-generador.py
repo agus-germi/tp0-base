314f6e20e278582c3ed02d69ac40f23c421e295e
agus-germi <agerminario@fi.uba.ar>
2025-08-28 20:15:06 -0300

    refactor: change compose generator to add csv volume

--- mi-generador.py
+++ mi-generador.py
@@ -1,20 +1,5 @@
 import sys
 
-def obtener_campo_aleatorio(dni=False, nro=False):
-    dnis = ["30904465", "43448326", "23456789", "34567890", "45678901"]
-    nros = ["7574", "6531", "9090", "3071", "2106"]
-    
-    if not hasattr(obtener_campo_aleatorio, "i"):
-      obtener_campo_aleatorio.i = 0
-    idx = obtener_campo_aleatorio.i % 5 #between 0 y 4 
-    obtener_campo_aleatorio.i += 1
-    
-    if dni:
-      return dnis[idx]
-    else:
-      return nros[idx]
-
-
 def generar_server(clients):
     """Genera la seccion del servidor en el YAML."""
     return f"""  server:
@@ -32,26 +17,19 @@ def generar_server(clients):
 
 def generar_client(i):
     """Genera la seccion de un cliente en el YAML."""
-    dni = obtener_campo_aleatorio(True, False)
-    nro_apostado = obtener_campo_aleatorio(False, True)
-
     return f"""  client{i}:
     container_name: client{i}
     image: client:latest
     entrypoint: /client
     environment:
       - CLI_ID={i}
-      - CLI_NOMBRE=Santiago Lionel
-      - CLI_APELLIDO=Lorca
-      - CLI_DNI={dni}
-      - CLI_NACIMIENTO=1999-03-17
-      - CLI_NUMERO={nro_apostado}
     networks:
       - testing_net
     depends_on:
       - server
     volumes:
       - ./client/config.yaml:/config.yaml
+      - ./data/dataset/agency-{i}.csv:/bets.csv
 """
 
 def generar_red():
